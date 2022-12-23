e229ff5d71c5019dcb3ab98bd9ecc071a34e14e0
niteesh342692 <91664298+niteesh342692@users.noreply.github.com>
2022-12-23 12:02:04 +0530

    Create changeState.sol

---
+++ changeState.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.5.9;
+
+contract changeState
+{
+	enum S {Init, Reg, Vote, Done}
+	S public st;
+	uint public timeNow;
+	uint startTime;
+
+	constructor()  public
+	{
+		timeNow=now;
+		st=S.Init;
+	}
+
+	function cs() public
+	{
+		timeNow=now;
+		if(timeNow>startTime+10 seconds)
+		{
+			startTime=timeNow;
+			if(st==S.Init){st=S.Reg;return;}
+			else if(st==S.Reg){st=S.Vote;return;}
+			else {st=S.Done;return;}
+
+		}
+		return;
+	}
+}
